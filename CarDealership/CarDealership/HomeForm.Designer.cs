359834ea238b5c1f3eb3999e51d4cb318df350b8
Mendokk007 <kobemendoza30@gmail.com>
2026-03-11 04:59:49 +0800

    Add files via upload

---
+++ CarDealership/CarDealership/HomeForm.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace CarDealership
+{
+    partial class HomeForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Panel pnlTopBar;
+        private System.Windows.Forms.Label lblSystemTitle;
+        private System.Windows.Forms.Label lblUserGreeting;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Panel pnlCarousel;
+        private System.Windows.Forms.PictureBox pbCarImage;
+        private System.Windows.Forms.Label lblCarName;
+        private System.Windows.Forms.Label lblCarPrice;
+        private System.Windows.Forms.Timer timerCarousel;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnSelect;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlTopBar = new System.Windows.Forms.Panel();
+            this.lblSystemTitle = new System.Windows.Forms.Label();
+            this.lblUserGreeting = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.pnlCarousel = new System.Windows.Forms.Panel();
+            this.pbCarImage = new System.Windows.Forms.PictureBox();
+            this.lblCarName = new System.Windows.Forms.Label();
+            this.lblCarPrice = new System.Windows.Forms.Label();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.timerCarousel = new System.Windows.Forms.Timer(this.components);
+            this.pnlTopBar.SuspendLayout();
+            this.pnlCarousel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCarImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTopBar
+            // 
+            this.pnlTopBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.pnlTopBar.Controls.Add(this.lblSystemTitle);
+            this.pnlTopBar.Controls.Add(this.lblUserGreeting);
+            this.pnlTopBar.Controls.Add(this.btnLogout);
+            this.pnlTopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTopBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTopBar.Name = "pnlTopBar";
+            this.pnlTopBar.Size = new System.Drawing.Size(800, 80);
+            this.pnlTopBar.TabIndex = 0;
+            // 
+            // lblSystemTitle
+            // 
+            this.lblSystemTitle.AutoSize = true;
+            this.lblSystemTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblSystemTitle.ForeColor = System.Drawing.Color.White;
+            this.lblSystemTitle.Location = new System.Drawing.Point(20, 15);
+            this.lblSystemTitle.Name = "lblSystemTitle";
+            this.lblSystemTitle.Size = new System.Drawing.Size(172, 30);
+            this.lblSystemTitle.TabIndex = 1;
+            this.lblSystemTitle.Text = "2F SUPER CARS";
+            // 
+            // lblUserGreeting
+            // 
+            this.lblUserGreeting.AutoSize = true;
+            this.lblUserGreeting.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblUserGreeting.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(187)))), ((int)(((byte)(190)))));
+            this.lblUserGreeting.Location = new System.Drawing.Point(22, 50);
+            this.lblUserGreeting.Name = "lblUserGreeting";
+            this.lblUserGreeting.Size = new System.Drawing.Size(99, 19);
+            this.lblUserGreeting.TabIndex = 2;
+            this.lblUserGreeting.Text = "Welcome, User";
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.btnLogout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(690, 40);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(90, 30);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // pnlCarousel
+            // 
+            this.pnlCarousel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.pnlCarousel.Controls.Add(this.pbCarImage);
+            this.pnlCarousel.Controls.Add(this.lblCarName);
+            this.pnlCarousel.Controls.Add(this.lblCarPrice);
+            this.pnlCarousel.Controls.Add(this.btnPrevious);
+            this.pnlCarousel.Controls.Add(this.btnNext);
+            this.pnlCarousel.Controls.Add(this.btnSelect);
+            this.pnlCarousel.Location = new System.Drawing.Point(50, 100);
+            this.pnlCarousel.Name = "pnlCarousel";
+            this.pnlCarousel.Size = new System.Drawing.Size(700, 390);
+            this.pnlCarousel.TabIndex = 4;
+            // 
+            // pbCarImage
+            // 
+            this.pbCarImage.BackColor = System.Drawing.Color.Transparent;
+            this.pbCarImage.Location = new System.Drawing.Point(50, 30);
+            this.pbCarImage.Name = "pbCarImage";
+            this.pbCarImage.Size = new System.Drawing.Size(600, 250);
+            this.pbCarImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCarImage.TabIndex = 0;
+            this.pbCarImage.TabStop = false;
+            // 
+            // lblCarName
+            // 
+            this.lblCarName.AutoSize = true;
+            this.lblCarName.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblCarName.ForeColor = System.Drawing.Color.White;
+            this.lblCarName.Location = new System.Drawing.Point(45, 290);
+            this.lblCarName.Name = "lblCarName";
+            this.lblCarName.Size = new System.Drawing.Size(120, 30);
+            this.lblCarName.TabIndex = 1;
+            this.lblCarName.Text = "Car Model";
+            // 
+            // lblCarPrice
+            // 
+            this.lblCarPrice.AutoSize = true;
+            this.lblCarPrice.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblCarPrice.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.lblCarPrice.Location = new System.Drawing.Point(500, 290);
+            this.lblCarPrice.Name = "lblCarPrice";
+            this.lblCarPrice.Size = new System.Drawing.Size(61, 25);
+            this.lblCarPrice.TabIndex = 2;
+            this.lblCarPrice.Text = "$0.00";
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.btnPrevious.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPrevious.FlatAppearance.BorderSize = 0;
+            this.btnPrevious.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrevious.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnPrevious.ForeColor = System.Drawing.Color.White;
+            this.btnPrevious.Location = new System.Drawing.Point(50, 340);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(100, 35);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "◀ Previous";
+            this.btnPrevious.UseVisualStyleBackColor = false;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            this.btnPrevious.MouseEnter += new System.EventHandler(this.btnPrevious_MouseEnter);
+            this.btnPrevious.MouseLeave += new System.EventHandler(this.btnPrevious_MouseLeave);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.btnNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNext.FlatAppearance.BorderSize = 0;
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnNext.ForeColor = System.Drawing.Color.White;
+            this.btnNext.Location = new System.Drawing.Point(550, 340);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(100, 35);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "Next ▶";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            this.btnNext.MouseEnter += new System.EventHandler(this.btnNext_MouseEnter);
+            this.btnNext.MouseLeave += new System.EventHandler(this.btnNext_MouseLeave);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.btnSelect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSelect.FlatAppearance.BorderSize = 0;
+            this.btnSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelect.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSelect.ForeColor = System.Drawing.Color.White;
+            this.btnSelect.Location = new System.Drawing.Point(275, 340);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(150, 40);
+            this.btnSelect.TabIndex = 5;
+            this.btnSelect.Text = "SELECT CAR";
+            this.btnSelect.UseVisualStyleBackColor = false;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            this.btnSelect.MouseEnter += new System.EventHandler(this.btnSelect_MouseEnter);
+            this.btnSelect.MouseLeave += new System.EventHandler(this.btnSelect_MouseLeave);
+            // 
+            // timerCarousel
+            // 
+            this.timerCarousel.Interval = 8000;
+            this.timerCarousel.Tick += new System.EventHandler(this.timerCarousel_Tick);
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.pnlCarousel);
+            this.Controls.Add(this.pnlTopBar);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "HomeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard - Car Dealership";
+            this.pnlTopBar.ResumeLayout(false);
+            this.pnlTopBar.PerformLayout();
+            this.pnlCarousel.ResumeLayout(false);
+            this.pnlCarousel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCarImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ CarDealership/CarDealership/HomeForm.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CarDealership
+{
+    public partial class HomeForm : Form
+    {
+        private string _connectionString;
+        private string _username;
+        private List<Car> cars;
+        private int currentIndex = 0;
+
+        public HomeForm(string connectionString, string username)
+        {
+            InitializeComponent();
+            _connectionString = connectionString;
+            _username = username;
+            lblUserGreeting.Text = "Welcome, " + username;
+
+            ApplyRedTheme();
+
+            pbCarImage.BackColor = Color.Transparent;
+            pbCarImage.SizeMode = PictureBoxSizeMode.Zoom;
+
+            LoadCars();
+        }
+
+        private void ApplyRedTheme()
+        {
+            btnLogout.MouseEnter += (s, e) => btnLogout.BackColor = Color.FromArgb(217, 55, 55);
+            btnLogout.MouseLeave += (s, e) => btnLogout.BackColor = Color.FromArgb(240, 71, 71);
+
+            btnSelect.MouseEnter += (s, e) => btnSelect.BackColor = Color.FromArgb(217, 55, 55);
+            btnSelect.MouseLeave += (s, e) => btnSelect.BackColor = Color.FromArgb(240, 71, 71);
+
+            btnPrevious.MouseEnter += (s, e) => btnPrevious.BackColor = Color.FromArgb(94, 99, 107);
+            btnPrevious.MouseLeave += (s, e) => btnPrevious.BackColor = Color.FromArgb(64, 68, 75);
+
+            btnNext.MouseEnter += (s, e) => btnNext.BackColor = Color.FromArgb(94, 99, 107);
+            btnNext.MouseLeave += (s, e) => btnNext.BackColor = Color.FromArgb(64, 68, 75);
+        }
+
+        private void LoadCars()
+        {
+            cars = new List<Car>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT CarID, ModelName, Price, ImageUrl, Year, Description FROM Cars";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            cars.Add(new Car
+                            {
+                                CarID = (int)reader["CarID"],
+                                ModelName = reader["ModelName"].ToString(),
+                                Price = (decimal)reader["Price"],
+                                ImageUrl = reader["ImageUrl"]?.ToString() ?? "",
+                                Year = reader["Year"] != DBNull.Value ? (int)reader["Year"] : 0,
+                                Description = reader["Description"]?.ToString() ?? ""
+                            });
+                        }
+                    }
+                }
+
+                if (cars.Count > 0)
+                {
+                    ShowCar(currentIndex);
+                    timerCarousel.Start();
+                }
+                else
+                {
+                    MessageBox.Show("No cars available to display.", "Information",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading cars: " + ex.Message, "Database Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ShowCar(int index)
+        {
+            if (cars == null || cars.Count == 0) return;
+
+            try
+            {
+                var car = cars[index];
+                lblCarName.Text = $"{car.ModelName} {(car.Year > 0 ? $"({car.Year})" : "")}";
+                lblCarPrice.Text = car.Price.ToString("C");
+
+                if (pbCarImage.Image != null)
+                {
+                    pbCarImage.Image.Dispose();
+                    pbCarImage.Image = null;
+                }
+
+                if (!string.IsNullOrEmpty(car.ImageUrl))
+                {
+                    LoadImageFromFile(car.ImageUrl);
+                }
+                else
+                {
+                    CreatePlaceholderImage(car.ModelName, "No image path");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error displaying car: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadImageFromFile(string imagePath)
+        {
+            try
+            {
+                string fullPath = Path.Combine(Application.StartupPath, imagePath);
+
+                if (!File.Exists(fullPath))
+                {
+                    string fileName = Path.GetFileName(imagePath);
+                    fullPath = Path.Combine(Application.StartupPath, "CarImages", fileName);
+                }
+
+                if (File.Exists(fullPath))
+                {
+                    using (FileStream fs = new FileStream(fullPath, FileMode.Open, FileAccess.Read))
+                    {
+                        Image original = Image.FromStream(fs);
+
+                        Bitmap bmp = new Bitmap(original.Width, original.Height, PixelFormat.Format32bppArgb);
+                        using (Graphics g = Graphics.FromImage(bmp))
+                        {
+                            g.Clear(Color.Transparent);
+                            g.DrawImage(original, 0, 0, original.Width, original.Height);
+                        }
+
+                        pbCarImage.Image = bmp;
+                        original.Dispose();
+                    }
+                }
+                else
+                {
+                    CreatePlaceholderImage("Image Not Found", Path.GetFileName(imagePath));
+                }
+            }
+            catch (Exception ex)
+            {
+                CreatePlaceholderImage("Error", ex.Message);
+            }
+        }
+
+        private void CreatePlaceholderImage(string line1, string line2)
+        {
+            Bitmap placeholder = new Bitmap(600, 250);
+            using (Graphics g = Graphics.FromImage(placeholder))
+            {
+                g.Clear(Color.FromArgb(64, 68, 75));
+                using (Font font1 = new Font("Segoe UI", 16, FontStyle.Bold))
+                using (Font font2 = new Font("Segoe UI", 10))
+                using (Brush brush = new SolidBrush(Color.White))
+                using (Brush brush2 = new SolidBrush(Color.FromArgb(185, 187, 190)))
+                {
+                    StringFormat sf = new StringFormat();
+                    sf.Alignment = StringAlignment.Center;
+                    sf.LineAlignment = StringAlignment.Center;
+
+                    g.DrawString(line1, font1, brush, new Rectangle(0, 80, 600, 50), sf);
+                    g.DrawString(line2, font2, brush2, new Rectangle(0, 140, 600, 30), sf);
+                }
+            }
+            pbCarImage.Image = placeholder;
+        }
+
+        private void timerCarousel_Tick(object sender, EventArgs e)
+        {
+            if (cars == null || cars.Count == 0) return;
+            currentIndex = (currentIndex + 1) % cars.Count;
+            ShowCar(currentIndex);
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (cars == null || cars.Count == 0) return;
+            timerCarousel.Stop();
+            currentIndex--;
+            if (currentIndex < 0) currentIndex = cars.Count - 1;
+            ShowCar(currentIndex);
+            timerCarousel.Start();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (cars == null || cars.Count == 0) return;
+            timerCarousel.Stop();
+            currentIndex = (currentIndex + 1) % cars.Count;
+            ShowCar(currentIndex);
+            timerCarousel.Start();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            if (cars == null || cars.Count == 0)
+            {
+                MessageBox.Show("No cars available to select.", "Information",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            Car selectedCar = cars[currentIndex];
+            PurchaseForm purchaseForm = new PurchaseForm(selectedCar, _username);
+            purchaseForm.ShowDialog();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to logout?",
+                "Confirm Logout", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                LoginForm login = new LoginForm();
+                login.Show();
+                this.Close();
+            }
+        }
+
+        // Navigation button hover effects
+        private void btnPrevious_MouseEnter(object sender, EventArgs e) => btnPrevious.BackColor = Color.FromArgb(94, 99, 107);
+        private void btnPrevious_MouseLeave(object sender, EventArgs e) => btnPrevious.BackColor = Color.FromArgb(64, 68, 75);
+        private void btnNext_MouseEnter(object sender, EventArgs e) => btnNext.BackColor = Color.FromArgb(94, 99, 107);
+        private void btnNext_MouseLeave(object sender, EventArgs e) => btnNext.BackColor = Color.FromArgb(64, 68, 75);
+        private void btnSelect_MouseEnter(object sender, EventArgs e) => btnSelect.BackColor = Color.FromArgb(217, 55, 55);
+        private void btnSelect_MouseLeave(object sender, EventArgs e) => btnSelect.BackColor = Color.FromArgb(240, 71, 71);
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                Application.Exit();
+            }
+        }
+    }
+
+    public class Car
+    {
+        public int CarID { get; set; }
+        public string ModelName { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int Year { get; set; }
+        public string Description { get; set; }
+    }
+}
